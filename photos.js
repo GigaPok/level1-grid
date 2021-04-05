f9637d7c47387555a69192074c20728e5a7ec9c1
Giga Pok <gigapokera@gmail.comm>
2021-04-05 18:43:28 +0400

    fix-gallery

---
+++ photos.js
@@ -0,0 +1,6 @@
+// var images = ['2.jpg', '3.jpg', '4.jpg', '2.jpg',
+//   '2.jpg', '2.jpg'];
+//   document.getElementsByClassName('img')[0].style.backgroundImage = 'url(../img/' + images[Math.floor(Math.random() * images.length)] + ')';
+//   console.log(images);
+
+
